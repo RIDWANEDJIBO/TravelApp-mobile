9e36b344f49172ab0684d5bb555c5b8f50064cc7
Djibo Alhassane Ridwane <djiboridwanealhassane@gmail.com>
2024-11-24 00:46:20 +0100

    fourth commit: DetailHotel activity in the project

--- app/src/main/java/fr/mycompany/travelapp/ui/resultats/DetailHotelActivity.java
+++ app/src/main/java/fr/mycompany/travelapp/ui/resultats/DetailHotelActivity.java
@@ -1,4 +1,4 @@
-package fr.mycompany.travelapp;
+package fr.mycompany.travelapp.ui.resultats;
 
 import android.os.Bundle;
 
@@ -8,7 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import fr.mycompany.travelapp.R;
+
+public class DetailHotelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
